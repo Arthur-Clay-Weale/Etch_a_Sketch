af8c8c119cdc0f6ecb41c1da3596610fecbd17cd
Arthur Weale <ajayrossy340@gmail.com>
2024-12-22 19:49:20 +0200

    Adding hover effects

--- etch.js
+++ etch.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 let boxes = [];
-container.style.cssText = "border: 2px solid blue;display: flex; flex-wrap : wrap; align-item: center; width: 1445px";
+container.style.cssText = "border: 2px solid blue;display: flex; flex-wrap : wrap; align-item: center; width: 95px; box-sizing: border-box";
 
 function createDivs (){
     for(let i = 0; i < 256 ;i++){
@@ -8,8 +8,15 @@ function createDivs (){
         boxes.push(box);
         console.log(boxes[i]);
         container.append(box);
-        box.style.cssText = "border : 2px solid green; height : 80px; width : 80px; margin: 3px; align-item: center";
+        box.style.cssText = "border : 1px solid black; height : 2px; width : 2px; margin: 1px; align-item: center";
     }
+        for(eachbox of boxes){
+            box.addEventListener("mouseenter", () => {
+                box.style.cssText = "background: black";
+            })
+        }
 }
 
-createDivs();
+
+
+createDivs(); 
