7a8223a10b47f2e6ccbf56d1dd87fcdc4aa6186c
Arthur Weale <ajayrossy340@gmail.com>
2024-12-22 16:22:58 +0200

    Arrange the squares 16 by 16 order

--- etch.js
+++ etch.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#container");
 let boxes = [];
-container.style.cssText = "5px solid blue";
+container.style.cssText = "border: 2px solid blue;display: flex; flex-wrap : wrap; align-item: center; width: 1445px";
 
 function createDivs (){
     for(let i = 0; i < 256 ;i++){
@@ -8,6 +8,7 @@ function createDivs (){
         boxes.push(box);
         console.log(boxes[i]);
         container.append(box);
+        box.style.cssText = "border : 2px solid green; height : 80px; width : 80px; margin: 3px; align-item: center";
     }
 }
 
